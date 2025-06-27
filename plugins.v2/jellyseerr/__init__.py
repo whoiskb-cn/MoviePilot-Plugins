6d64da2a1e34273aff9b086eb5ff8b982d314704
whoiskb-cn <63866719+whoiskb-cn@users.noreply.github.com>
2025-06-27 16:30:46 +0800

    __init__.py

--- plugins.v2/jellyseerr/__init__.py
+++ plugins.v2/jellyseerr/__init__.py
@@ -1,1 +1,15 @@
+from app.plugins import PluginBase
+from .handler import register_webhook_handler
+
+class Plugin(PluginBase):
+    def init_plugin(self, config: dict = None):
+        register_webhook_handler(config)
+
+    def get_plugin_info(self):
+        return {
+            "name": "Jellyseerr通知转发",
+            "description": "监听Jellyseerr Webhook并推送通知到微信",
+            "author": "whoiskb",
+            "version": "1.0"
+        }
 
